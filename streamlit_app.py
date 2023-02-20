816a5b47eebefd08f26f4524639145f429b0ed94
= <charlie.spackman@dtsquared.co.uk>
2023-02-20 16:03:32 +0000

    Full snowpark version completed

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,15 +14,9 @@ def create_session_object():
 
 conn = create_session_object()
 
-# Function to perform query.
-# Uses st.experimental_memo to only rerun when the query changes or after 10 min.
-@st.experimental_memo(ttl=600)
-def run_query(query: str):
-    data = conn.sql(query)
-    return data
 
 # Get a unique list of products to select from
-products = run_query("SELECT DISTINCT COMMODITY_DESC from CHARLIE_FEATURE_STORE WHERE COMMODITY_DESC != 'ADULT INCONTINENCE' ORDER BY COMMODITY_DESC").to_pandas()
+products = conn.sql("SELECT DISTINCT COMMODITY_DESC from CHARLIE_FEATURE_STORE WHERE COMMODITY_DESC != 'ADULT INCONTINENCE' ORDER BY COMMODITY_DESC").to_pandas()
 
 st.title("Propensity to Buy")
 st.caption("👋 Hello, welcome to our customer propensity scoring app! Choose a product from the drop down below and then select a propensity score range using the blue toggle, the model will then generate a list of househouses and their propensity to buy the product you have selected.")
@@ -52,10 +46,10 @@ if run_model:
     st.text("👨🏼‍💻 Running the model...")
     
     # Push ML to Snowflake
-    run_query(f"CALL TRAIN_PROPENSITY_MODEL({product_selection})").collect()
+    conn.sql(f"CALL TRAIN_PROPENSITY_MODEL({product_selection})").collect()
 
     # Get predictions
-    data = run_query("SELECT * FROM CHARLIE_INFERENCE_PREDICTIONS").to_pandas()
+    data = conn.sql("SELECT * FROM CHARLIE_INFERENCE_PREDICTIONS").to_pandas()
 
     # Data for streamlit table
     display_data = data[['HOUSEHOLD_KEY', 'PREDICTION']].sort_values(by='PREDICTION', ascending= False)
